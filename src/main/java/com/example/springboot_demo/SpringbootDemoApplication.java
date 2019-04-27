8efd373cbcdf1f1fe1dbe6c13843544afd8b42a5
Thiemo Urselmann <thiemo.urselmann@googlemail.com>
2019-04-27 11:24:06 +0200

    Introduce main class as root controller

--- src/main/java/com/example/springboot_demo/SpringbootDemoApplication.java
+++ src/main/java/com/example/springboot_demo/SpringbootDemoApplication.java
@@ -2,9 +2,19 @@ package com.example.springboot_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @SpringBootApplication
+@RestController
 public class SpringbootDemoApplication {
+
+    @RequestMapping(value = "/", method = GET)
+    public String home() {
+        return "Hello Docker World";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootDemoApplication.class, args);
