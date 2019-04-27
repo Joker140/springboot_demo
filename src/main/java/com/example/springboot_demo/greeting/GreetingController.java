ff8ea5826eb58d79c12a86467a3fe39d2c11c46e
Thiemo Urselmann <thiemo.urselmann@googlemail.com>
2019-04-27 10:58:41 +0200

    Narrow request mapping for greeting

--- src/main/java/com/example/springboot_demo/greeting/GreetingController.java
+++ src/main/java/com/example/springboot_demo/greeting/GreetingController.java
@@ -6,13 +6,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
 @RestController
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
+    @RequestMapping(value = "/greeting", method = GET)
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
                 String.format(template, name));
